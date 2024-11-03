5f64522fee6aaa1a6210cd6fea99ab6cd3353071
ytls <2371083008@qq.com>
2024-11-03 18:05:55 +0800

    网关层基本业务逻辑框架

---
+++ src/main/java/icu/ytlsnb/alpacaapigateway/AlpacaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package icu.ytlsnb.alpacaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpacaApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlpacaApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/main/java/icu/ytlsnb/alpacaapigateway/filter/ProjectFilter.java
@@ -0,0 +1,124 @@
+package icu.ytlsnb.alpacaapigateway.filter;
+
+import cn.hutool.core.util.StrUtil;
+import icu.ytlsnb.alpacaapiclientsdk.utils.SignUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Component
+@Slf4j
+public class ProjectFilter implements GlobalFilter, Ordered {
+//    private final List<String> WHITE_LIST = Arrays.asList("127.0.0.1");
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 2. 请求日志
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("ID: {}", request.getId());
+        log.info("Path: {}", request.getPath().value());
+        log.info("Method: {}", request.getMethod());
+        log.info("Params: {}", request.getQueryParams());
+        String remoteAddress = request.getRemoteAddress().getHostString();
+        log.info("Remote Address: {}", remoteAddress);
+        log.info("-------------------------------");
+//        // 3. 黑白名单
+//        if (!WHITE_LIST.contains(remoteAddress)) {
+//            return exchange.getResponse().setComplete();
+//        }
+        // 4. 鉴权
+        HttpHeaders headers = request.getHeaders();
+        String accessKey = headers.getFirst("accessKey");
+        String nonce = headers.getFirst("nonce");
+        String timestamp = headers.getFirst("timestamp");
+        String sign = headers.getFirst("sign");
+        // TODO sk将来应该从数据库里查询
+        String secretKey = "ytls";
+        // TODO 这里的所有参数都应该校验
+        ServerHttpResponse response = exchange.getResponse();
+        // 时间不超过5分钟
+        long currentTime = System.currentTimeMillis() / 1000;
+        if (timestamp == null || (currentTime - Integer.parseInt(timestamp)) > 60 * 5) {
+            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);// 设置状态码
+            return exchange.getResponse().setComplete();// 拦截
+        }
+        HashMap<String, String> serverHeaders = new HashMap<>();
+        serverHeaders.put("accessKey", accessKey);
+        // TODO 这里的随机数可以保存在Redis、数据库等地方
+        serverHeaders.put("nonce", nonce);
+        serverHeaders.put("timestamp", timestamp);
+        String serverSign = SignUtils.getSign(serverHeaders, secretKey);
+        if (!serverSign.equals(sign)) {
+            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);// 设置状态码
+            return exchange.getResponse().setComplete();// 拦截
+        }
+        ServerHttpResponse originalResponse = exchange.getResponse();
+        DataBufferFactory bufferFactory = originalResponse.bufferFactory();
+        ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+            @Override
+            public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                if (body instanceof Flux) {
+                    Flux<? extends DataBuffer> fluxBody = Flux.from(body);
+                    return super.writeWith(fluxBody.map(dataBuffer -> {
+                        byte[] content = new byte[dataBuffer.readableByteCount()];
+                        dataBuffer.read(content);
+                        DataBufferUtils.release(dataBuffer);//释放掉内存
+                        // 构建日志
+                        StringBuilder sb = new StringBuilder(200);
+                        List<Object> rspArgs = new ArrayList<>();
+                        rspArgs.add(originalResponse.getStatusCode());
+                        /*---------------真正的响应数据---------------*/
+                        String data = new String(content, StandardCharsets.UTF_8);
+                        sb.append(data);
+                        // 7. 响应日志
+                        log.info("Response Data: {}", data);
+                        log.info("-------------------------------");
+                        // TODO 8. 调用成功，调用次数 + 1
+                        return bufferFactory.wrap(content);
+                    }));
+                } else {
+                    log.error("{} 响应code异常", getStatusCode());
+                }
+                return super.writeWith(body);
+            }
+        };
+        // 使用 onErrorResume 来捕获异步异常并设置自定义响应内容
+        return chain.filter(exchange.mutate().response(decoratedResponse).build())
+                .onErrorResume(e -> {
+                    // 9. 调用失败，返回一个规范的错误码
+                    log.error("The interface is offline...", e);
+                    response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+                    String errorMessage = "接口暂时不可用，请稍后重试！\n（绝对不是因为提供者跑路了！！！）";
+                    DataBuffer buffer = response.bufferFactory().wrap(errorMessage.getBytes(StandardCharsets.UTF_8));
+                    response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+                    return response.writeWith(Mono.just(buffer));
+                });
+    }
+
+    @Override
+    public int getOrder() {
+        return -2;
+    }
+}
